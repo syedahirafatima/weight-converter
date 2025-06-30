19e4834fffbdfd94a1eb980cddb6706c8afc1ad1
Syeda Hira Fatima <syedahirafatima11@gmail.com>
2025-06-30 21:35:13 +0500

    code.py

--- code.py
+++ code.py
@@ -9,12 +9,12 @@ choice = input("Enter 1 or 2 to choose conversion: ")
 if choice == "1":
     pounds = float(input("Enter weight in pounds: "))
     kilograms = pounds * 0.453592
-    print(f"{pounds} pounds is {kilograms:.2f} kilograms.")
+    print(pounds, "pounds is", round(kilograms, 2), "kilograms.")
     
 elif choice == "2":
     kilograms = float(input("Enter weight in kilograms: "))
     pounds = kilograms / 0.453592
-    print(f"{kilograms} kilograms is {pounds:.2f} pounds.")
+    print(kilograms, "kilograms is", round(pounds, 2), "pounds.")
     
 else:
     print("Invalid choice. Please enter 1 or 2.")
